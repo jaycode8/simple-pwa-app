ef43db44ca56ac06d8c05d1d9db3234548fc5de4
James Mumo <86493008+jaycode8@users.noreply.github.com>
2025-09-28 13:45:50 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,25 @@
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open("pwa-demo-cache").then(cache => {
-            return cache.addAll([
-                "./",
-                "./index.html",
-                "./manifest.json"
-            ]);
-        })
-    );
+const cacheName = "demo-v1";
+const assets = [
+  "/",
+  "/index.html",
+  "/imgs/logo.png",
+  "/script.js"
+];
+
+// Install event
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+// Fetch event
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
 });
